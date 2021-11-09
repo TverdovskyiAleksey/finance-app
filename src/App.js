6c72e9665edf1a0c9154d742fff8dd6515ffcf10
Alex <tverdovskyi92@gmail.com>
2021-11-10 01:48:01 +0200

    Create structure, added components, added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import Container from './Components/Container';
+import { Switch } from 'react-router-dom';
+import PublicRoute from './Routes/PublicRoute';
+import PrivateRoute from './Routes/PrivateRoute';
+import LoginView from './Views/LoginView';
+import RegisterView from './Views/RegisterView';
+import StatisticView from './Views/StatisticView';
+import HomeView from './Views/HomeView';
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container>
+      <Switch>
+        <PublicRoute exact path="/login" redirectTo="/home" restricted>
+          <LoginView />
+        </PublicRoute>
+        <PublicRoute exact path="/register" restricted>
+          <RegisterView />
+        </PublicRoute>
+        <PrivateRoute>
+          <HomeView exact path="/home" redirectTo="/login" />
+        </PrivateRoute>
+        <PrivateRoute>
+          <StatisticView exact path="/statistic" redirectTo="/login" />
+        </PrivateRoute>
+      </Switch>
+    </Container>
   );
 }
 
---
+++ src/Components/Statistic/Statistic.js
@@ -0,0 +1,5 @@
+const Statistic = () => {
+  return <div></div>;
+};
+
+export default Statistic;
