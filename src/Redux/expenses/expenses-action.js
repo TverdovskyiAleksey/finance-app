e1b463a06890d88db06e712a98c48f3d2b3004d2
Alex <tverdovskyi92@gmail.com>
2021-11-11 20:46:47 +0200

    Created redux

---
+++ src/Redux/expenses/expenses-action.js
@@ -0,0 +1,17 @@
+import { createAction } from '@reduxjs/toolkit';
+
+export const fetchExpenseRequest = createAction('expense/fetchExpenseRequest');
+export const fetchExpenseSuccess = createAction('expense/fetchExpenseSuccess');
+export const fetchExpenseError = createAction('expense/fetchExpenseError');
+
+export const addExpenseRequest = createAction('expense/addExpenseRequest');
+export const addExpenseSuccess = createAction('expense/addExpenseSuccess');
+export const addExpenseError = createAction('expense/addExpenseError');
+
+export const deleteExpenseRequest = createAction(
+  'expense/deleteExpenseRequest',
+);
+export const deleteExpenseSuccess = createAction(
+  'expense/deleteExpenseSuccess',
+);
+export const deleteExpenseError = createAction('expense/deleteExpenseError');
