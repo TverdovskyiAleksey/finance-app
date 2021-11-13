cda20a926f3f8c4ecfd25270953593111586421d
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-11-13 15:01:52 +0200

    fix table

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -19,25 +19,29 @@ const Expenses = () => {
           <span className={styles.currency}>₴</span> {total}</p>
       </div>
       <table className={styles.table}>
-        <tr className={styles.head}>
-          <td className={styles.item}>Дата</td>
-          <td className={styles.center}>Тип</td>
-          <td className={styles.item}>Категория</td>
-          <td className={styles.item}>Комментарий</td>
-          <td className={styles.center}>Сумма</td>
-          <td className={styles.center}>Баланс</td>
-        </tr>
-        {expenses.map(({ id, date, type, category, description, sum }) =>
-        (
-          <tr key={id} className={styles.row}>
-            <td className={styles.item}>{date}</td>
-            <td className={styles.center}>{type}</td>
-            <td className={styles.item}>{category}</td>
-            <td className={styles.item}>{description}</td>
-            <td className={type === '-' ? styles.dec : styles.inc}>{sum}</td>
-            <td className={styles.center}>{type === '-' ? total - sum : total + sum}</td>
+        <thead className={styles.head}>
+          <tr className={styles.headRow}>
+            <td className={styles.item}>Дата</td>
+            <td className={styles.center}>Тип</td>
+            <td className={styles.item}>Категория</td>
+            <td className={styles.item}>Комментарий</td>
+            <td className={styles.center}>Сумма</td>
+            <td className={styles.center}>Баланс</td>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {expenses.map(({ id, date, type, category, description, sum }) =>
+          (
+            <tr key={id} className={styles.row}>
+              <td className={styles.item}>{date}</td>
+              <td className={styles.center}>{type}</td>
+              <td className={styles.item}>{category}</td>
+              <td className={styles.item}>{description}</td>
+              <td className={type === '-' ? styles.dec : styles.inc}>{sum}</td>
+              <td className={styles.center}>{type === '-' ? total - sum : total + sum}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </>
   )
