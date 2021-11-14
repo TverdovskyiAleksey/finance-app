a4298df7009d7d3f4b598ae103e2b180af55cf69
Svetlana-Pishchulina <svetlana-plias@gmail.com>
2021-11-14 17:14:12 +0200

    add Modal

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -1,7 +1,21 @@
-const HomeView = () => (
+import Transaction from '../Components/Transaction';
+import Modal from '../Components/Modal';
+import { useState } from 'react';
+
+const HomeView = () => {
+  const [showModal, setShowModal] = useState(true);
+  const toggleModal = () => {
+    setShowModal(prevState => !prevState);
+  };
+
   <div>
     <h1>Welcome home</h1>
-  </div>
-);
+    {showModal && (
+      <Modal onClose={toggleModal}>
+        <Transaction closeModal={toggleModal}></Transaction>
+      </Modal>
+    )}
+  </div>;
+};
 
 export default HomeView;
