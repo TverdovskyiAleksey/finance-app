b7d5be4100b4ee720635a84ef8d78f1707efb7ef
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-11-14 22:36:03 +0200

    addButton

--- src/Views/HomeView.js
+++ src/Views/HomeView.js
@@ -1,9 +1,14 @@
 import Expenses from "../Components/Expenses";
+import { ReactComponent as AddIcon } from '../Images/Icons/add-btn.svg';
+import styles from '../Components/Expenses/Expenses.module.css';
 
 const HomeView = () => (
   <div>
     <h1>Welcome home</h1>
     <Expenses />
+    <button className={styles.button} aria-label="add expenses">
+      <AddIcon width="44" height="44" />
+    </button>
   </div>
 );
 
