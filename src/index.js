8bc31e630348ff44209192e328c1c8d20c6d069d
Yuliia Udalova <yuliiapronina69@gmail.com>
2021-11-16 15:24:39 +0200

    готовая разметка страницы регистрации

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'modern-normalize/modern-normalize.css';
 import { BrowserRouter } from 'react-router-dom';
+import { store } from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
