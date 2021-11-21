c4564635610a96bcca3461913fb432276d076426
Svetlana-Pishchulina <svetlana-plias@gmail.com>
2021-11-21 13:28:40 +0200

    не мерджить

--- src/redux/auth/auth-slice.js
+++ src/redux/auth/auth-slice.js
@@ -5,6 +5,7 @@ const initialState = {
   user: { name: null, email: null },
   token: null,
   isLoggedIn: false,
+  isRefreshing: false,
 };
 
 const authSlice = createSlice({
@@ -16,10 +17,26 @@ const authSlice = createSlice({
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
+    [authOperations.logIn.fulfilled](state, action) {
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+      state.isLoggedIn = true;
+    },
     [authOperations.logOut.fulfilled](state, action) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
+    },
+    [authOperations.fetchCarrentUser.pending](state) {
+      state.isRefreshing = true;
+    },
+    [authOperations.fetchCarrentUser.fulfilled](state, action) {
+      state.user = action.payload;
+      state.isLoggedIn = true;
+      state.isRefreshing = false;
+    },
+    [authOperations.fetchCarrentUser.rejected](state) {
+      state.isRefreshing = false;
     },
   },
 });
