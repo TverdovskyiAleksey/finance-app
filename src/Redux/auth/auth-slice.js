e4130aecf89019910ede10eccb74ca2ddf9c5e34
RomanSokurenko <sokurenko.roman82@gmail.com>
2021-11-21 10:47:09 +0200

    Save start

--- src/Redux/auth/auth-slice.js
+++ src/Redux/auth/auth-slice.js
@@ -5,7 +5,6 @@ const initialState = {
   user: { name: null, email: null },
   token: null,
   isLoggedIn: false,
-  isRefreshing: false,
 };
 
 const authSlice = createSlice({
@@ -17,26 +16,10 @@ const authSlice = createSlice({
       state.token = action.payload.token;
       state.isLoggedIn = true;
     },
-    [authOperations.logIn.fulfilled](state, action) {
-      state.user = action.payload.user;
-      state.token = action.payload.token;
-      state.isLoggedIn = true;
-    },
     [authOperations.logOut.fulfilled](state, action) {
       state.user = { name: null, email: null };
       state.token = null;
       state.isLoggedIn = false;
-    },
-    [authOperations.fetchCarrentUser.pending](state) {
-      state.isRefreshing = true;
-    },
-    [authOperations.fetchCarrentUser.fulfilled](state, action) {
-      state.user = action.payload;
-      state.isLoggedIn = true;
-      state.isRefreshing = false;
-    },
-    [authOperations.fetchCarrentUser.rejected](state) {
-      state.isRefreshing = false;
     },
   },
 });
