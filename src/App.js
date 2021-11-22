73c1c9cb53034d0621b58ef3f5d7e40df1283f68
Alex <tverdovskyi92@gmail.com>
2021-11-22 22:32:58 +0200

    changes

--- src/App.js
+++ src/App.js
@@ -9,10 +9,26 @@ import HomeView from './Views/HomeView';
 import CurrencyView from './Views/CurrencyView';
 import AppBar from './Components/AppBar';
 import './fonts.css';
+import { useEffect, Suspense, lazy } from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { authOperations, authSelectors } from './Redux/auth';
+
+// const LoginView = lazy(() => import('./Views/LoginView'));
+// const HomeView = lazy(() => import('./Views/HomeView'));
+// const RegisterView = lazy(() => import('./Views/RegisterView'));
+// const StatisticView = lazy(() => import('./Views/StatisticView'));
+// const CurrencyView = lazy(() => import('./Views/CurrencyView'));
 
 function App() {
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(authSelectors.getRefreshing);
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCarrentUser());
+  }, [dispatch]);
+
   return (
     <Container>
       <AppBar />
@@ -23,15 +39,15 @@ function App() {
         <PublicRoute exact path="/register" restricted>
           <RegisterView />
         </PublicRoute>
-        {/* <PrivateRoute> */}
-        <HomeView exact path="/home" redirectTo="/login" />
-        {/* </PrivateRoute> */}
-        {/* <PrivateRoute> */}
-        <StatisticView exact path="/statistic" redirectTo="/login" />
-        {/* </PrivateRoute> */}
-        {/* <PrivateRoute> */}
-        <CurrencyView exact path="/currency" redirectTo="/login" />
-        {/* </PrivateRoute> */}
+        <PrivateRoute>
+          <HomeView exact path="/home" />
+        </PrivateRoute>
+        <PrivateRoute>
+          <StatisticView exact path="/statistic" />
+        </PrivateRoute>
+        <PrivateRoute>
+          <CurrencyView exact path="/currency" />
+        </PrivateRoute>
       </Switch>
       <ToastContainer />
     </Container>
