17957e51b2f0452576e707dc9673978c4a5ceec6
Yuliia Udalova <yuliiapronina69@gmail.com>
2021-11-22 14:53:12 +0200

    добавила валидацию

--- src/Views/LoginView.js
+++ src/Views/LoginView.js
@@ -1,7 +1,7 @@
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
-// import { Formik } from 'formik';
-// import * as yup from 'yup';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 import { authOperations } from '../Redux/auth';
 import { Link } from 'react-router-dom';
 import styles from '../Components/Auth/Auth.module.css';
@@ -13,25 +13,34 @@ import mail from '../Components/Auth/images/email.svg';
 
 export default function LoginView() {
   const dispatch = useDispatch();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
 
-  const handelChange = ({ target: { name, value } }) => {
-    switch (name) {
-      case 'email':
-        return setEmail(value);
-      case 'password':
-        return setPassword(value);
-      default:
-        return;
-    }
-  };
-  const handelSubmit = e => {
-    e.preventDefault();
-    dispatch(authOperations.logIn({ email, password }));
-    setEmail('');
-    setPassword('');
-  };
+  // const handelChange = ({ target: { name, value } }) => {
+  //   switch (name) {
+  //     case 'email':
+  //       return setEmail(value);
+  //     case 'password':
+  //       return setPassword(value);
+  //     default:
+  //       return;
+  //   }
+  // };
+  // const handelSubmit = e => {
+  //   e.preventDefault();
+  //   dispatch(authOperations.logIn({ email, password }));
+  //   setEmail('');
+  //   setPassword('');
+  // };
+  const validationSchema = yup.object().shape({
+    
+    password:   yup.string().matches(
+            /[0-9a-zA-Z!@#$%^&*]/,
+            'Недопустимые символы для пароля'
+    ).min(6, 'Пароль должен быть не менее 6 символов').max(12, 'Максимальная длина пароля 12 символов').typeError('Должно быть строкой').required('Обязательноe поле'),
+    
+    email: yup.string().email('Введите корректный email').required('Обязательноe поле')
+  })
   return (
     <div className={styles.register}>
       <div className={styles.regLeft}>
@@ -39,41 +48,56 @@ export default function LoginView() {
         <p className={styles.regTitle}>Finance App</p>
       </div>
       <div className={styles.regRight}>
-        <div className={styles.registerForm}>
-          <form
-            className={styles.form}
-            onSubmit={handelSubmit}
-            autoComplete="off"
-          >
-            <div className={styles.logo}>
-              <img className={styles.logoImg} src={logo} alt="logo" />
-              <p className={styles.title}>Wallet</p>
-            </div>
-
-            <div>
-              <input
+        <Formik
+          initialValues={{
+           
+            email: '',
+            password: ''
+          }}
+          validateOnBlur
+          onSubmit={({ email, password }, { resetForm }) => {
+                dispatch(authOperations.register({ email, password}))
+                resetForm();
+          }}
+          validationSchema={validationSchema}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, isValid, handleSubmit, dirty }) => (
+            <div className={styles.registerForm}>
+      
+        <form className={styles.form} onSubmit={handleSubmit} autoComplete="off">
+          <div className={styles.logo}>
+            <img className={styles.logoImg} src={logo} alt='logo' />
+             <p className={styles.title}>Wallet</p>
+          </div>
+       
+            <div >
+            <input
                 className={styles.input}
-                style={{ backgroundImage: 'url(' + mail + ')' }}
-                type="email"
-                name="email"
-                value={email}
-                placeholder="E-mail"
-                onChange={handelChange}
-              />
-            </div>
-            <div>
-              <input
+                style={{backgroundImage: "url(" + mail + ")"}}
+              type="email"
+              name="email"
+              value={values.email}
+              placeholder='E-mail'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {touched.email && errors.email && <p className={styles.error}>{errors.email}</p>}
+        </div>
+        <div>
+            <input
                 className={styles.input}
-                style={{ backgroundImage: 'url(' + pas + ')' }}
-                type="password"
-                name="password"
-                value={password}
-                placeholder="Пароль"
-                onChange={handelChange}
-              />
-            </div>
-
-            <div className={styles.btn}>
+                 style={{backgroundImage: "url(" + pas + ")"}}
+              type="password"
+              name="password"
+              value={values.password}
+              placeholder='Пароль'
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                  {touched.password && errors.password && <p className={styles.error}>{errors.password}</p>}
+        </div>
+           
+          <div className={styles.btn}>
               <button className={styles.btnReg} type="submit">
                 Вход
               </button>
@@ -81,8 +105,12 @@ export default function LoginView() {
                 Регистрация
               </Link>
             </div>
-          </form>
-        </div>
+      </form>
+     
+    </div>
+          )}
+
+    </Formik>
       </div>
     </div>
   );
